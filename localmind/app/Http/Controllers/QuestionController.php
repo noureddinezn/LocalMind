ebc1f8ec8df7e220abeb5c81e872e5abcb803a4f
noureddinezn <noureddinezouana@gmail.com>
2026-02-01 17:55:26 +0100

    creation de resonsecontroller

--- localmind/app/Http/Controllers/QuestionController.php
+++ localmind/app/Http/Controllers/QuestionController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
+use App\Models\Favorite;
 use Illuminate\Http\Request;
 
-class QuestionController extends Controller
+class FavoriteController extends Controller
 {
-    //
+    public function toggle($questionId)
+    {
+        $question = Question::findOrFail($questionId);
+        $user = auth()->user();
+
+        $favorite = Favorite::where('user_id', $user->id)
+            ->where('question_id', $question->id)
+            ->first();
+
+        if ($favorite) {
+            $favorite->delete();
+            $message = 'Question retirée des favoris.';
+        } else {
+            Favorite::create([
+                'user_id' => $user->id,
+                'question_id' => $question->id,
+            ]);
+            $message = 'Question ajoutée aux favoris !';
+        }
+
+        return back()->with('success', $message);
+    }
+
+    public function index()
+    {
+        $favorites = auth()->user()->favorites()
+            ->with('question.user')
+            ->latest()
+            ->paginate(10);
+
+        return view('questions.favorites', compact('favorites'));
+    }
 }
